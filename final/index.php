71e892767c890349bb918fab079339a9a393e4da
elroeyayiso <86745767+elroeyayiso@users.noreply.github.com>
2022-03-23 17:34:54 -0400

    Add files via upload

---
+++ final/index.php
@@ -0,0 +1,87 @@
+
+<!doctype html>
+<html lang= "en">
+<head>
+    <meta charset= "UTF-8">
+    <title> Switch classwork Exerise </title>
+    <link rel="stylesheet" href="home.css">
+</head>
+
+
+</head>
+<body>
+<header>
+<div id= "inner-header">
+ <a href="index.php"></a>
+ <img src="img/log.jpg" alt="logo"
+
+ width="100" height="100">
+
+
+
+ 
+</a>
+
+    <nav>
+        <ul>
+            
+            <li><a href= "index.php"> Home</a></li>
+            <li><a href= "about.php"> About</a></li>
+            <li><a href= "daily.php"> Daily</a></li>
+            <li><a href= "Contact.php">Contact</a></li>
+
+
+
+           
+        </ul>
+    </nav>
+
+    
+
+
+
+
+
+
+
+</header>
+
+<h1>
+    
+<p style="text-align:center;">Best cities for retirees </p>
+ </h1>
+
+ 
+ <p style="text-align:center;">
+This page would display cities that have housing affordability, happiness, desirability,
+</p>
+<p style="text-align:center;">
+ retiree taxes, the job market and access to quality health care, and is weighted based 
+</p>
+
+<p style="text-align:center;">
+ on a U.S. You might be able to save money and improve your quality of life by relocating
+</p>
+<p style="text-align:center;">
+ to a retirement spot that better suits your interests and budget. Many of our best retirement
+</p>
+<p style="text-align:center;">
+ cities for 2022 are located in Florida, a popular retirement location for decades. The weather 
+</p>
+<p style="text-align:center;">
+ is ideal for year-round activities and often for physical health. If you decide to work while 
+</p>
+<p style="text-align:center;">
+ retired in Florida, you won't pay state income tax.
+</p>
+    
+
+
+<p style="text-align:center;"><img src="Image/Best.jpg" alt="photo" width="500" height="450">
+
+</p>
+
+
+
+
+
