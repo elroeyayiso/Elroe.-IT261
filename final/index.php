0347bf9ed22df97f628b8aaf9614ea629439c15b
elroeyayiso <86745767+elroeyayiso@users.noreply.github.com>
2022-03-23 17:58:08 -0400

    Update index.php

--- final/index.php
+++ final/index.php
@@ -26,7 +26,7 @@
         <ul>
             
             <li><a href= "index.php"> Home</a></li>
-            <li><a href= "about.php"> About</a></li>
+            <li><a href= "About.php"> About</a></li>
             <li><a href= "daily.php"> Daily</a></li>
             <li><a href= "contact.php">Contact</a></li>
 
