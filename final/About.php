645e2e0037ed9e49bfd3be55a1a104706d7d9df2
elroeyayiso <86745767+elroeyayiso@users.noreply.github.com>
2022-03-24 01:11:03 -0700

    Update About.php

--- final/About.php
+++ final/About.php
@@ -42,7 +42,7 @@
 
 </header>
 
-<p style="text-align:center;"><img src="pics/img1.jpg" alt="photo" width="550" height="450">
+<p style="text-align:center;"><img src="pics/img1.jpg" alt="photo" width="500" height="450">
 </p>
 <p style="text-align:center;"><img src="pics/img.jpg" alt="photo" width="500" height="450">
 </p>
